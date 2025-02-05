938237b78c8bb1c348bb231f2705065fe8887f35
Johnson <ckt0922@gmail.com>
2025-02-05 22:17:47 +0800

    Lab to validate git remote repository

--- firstpython.py
+++ firstpython.py
@@ -1,2 +1,10 @@
 #Assignment 1 - Creating new Python file in GitHub
 print ("Python File added to the GitHub")
+
+#Assingment 2 - 
+import urllib.request, urllib.parse, urllib.error
+import ssl, json, http, sqlite3
+
+url = "https://github.com/johnsonktchan/githubtest.git"
+
+data = urllib.request.urlopen(url)
